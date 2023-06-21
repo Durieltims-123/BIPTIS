e3ef56ecd05dacfbd173149132d3f19e45bc496c
jonray <tacudog.jonray@gmail.com>
2023-06-21 08:18:39 +0800

    Summary Report Update

--- app/Http/Controllers/SummaryReportController.php
+++ app/Http/Controllers/SummaryReportController.php
@@ -2,56 +2,175 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class SummaryReportController extends Controller
 {
-    function generateSummaryReport(){
-    $links = getUserLinks();
-    $user_privilege = getUserPrivilege();
+    function generateSummaryReport()
+    {
+        $links = getUserLinks();
+        $user_privilege = getUserPrivilege();
 
-    return view("admin.generate_summary_report", ['links' => $links, 'user_privilege' => $user_privilege]);
+        return view("admin.generate_summary_report", ['links' => $links, 'user_privilege' => $user_privilege]);
     }
 
-    function getReportbyMonthYear(Request $request){
+    function getReportbyMonthYear(Request $request)
+    {
         $month = $request->month;
         $year = $request->year;
-        $keyword = $year."-".$month."%";
+        $keyword = $year . "-" . $month . "%";
 
         $project_complete = DB::table('project_plans')
-        ->join('procacts','latest_procact_id','=','procacts.procact_id')
-        ->join('project_activity_status','procacts.procact_id','project_activity_status.procact_id')
-        ->where('status','completed')
-        ->where('procacts.proceed_notice','like',$keyword)-> count();
+            ->join('procacts', 'latest_procact_id', '=', 'procacts.procact_id')
+            ->join('project_activity_status', 'procacts.procact_id', 'project_activity_status.procact_id')
+            ->where('status', 'completed')
+            ->where('procacts.proceed_notice', 'like', $keyword)->count();
 
-        $project_for_rebid =DB::table('project_logs')
-        ->where('log_date','like',$keyword)
-        ->where('project_log_type','like','Another SVP%')
-        ->orWhere('project_log_type','like','Program for Rebid%')
-        ->count();
+        $project_for_rebid = DB::table('project_logs')
+            ->where('log_date', 'like', $keyword)
+            ->where('project_log_type', 'like', 'Another SVP%')
+            ->orWhere('project_log_type', 'like', 'Program for Rebid%')
+            ->count();
 
         $project_for_review = DB::table('project_logs')
-        ->where('log_date','like',$keyword)
-        ->where('project_log_type','like','Project for Review%')
-        ->count();
+            ->where('log_date', 'like', $keyword)
+            ->where('project_log_type', 'like', 'Project for Review%')
+            ->count();
 
         $project_onprocess = DB::table('project_plans')
-        ->join('procacts','latest_procact_id','=','procacts.procact_id')
-        ->join('project_activity_status','procacts.procact_id','project_activity_status.procact_id')
-        ->select('latest_procact_id')
-        ->where([['procacts.bid_evaluation','<=',$year.'-'.$month.'-31'],['procacts.proceed_notice',null]])
-        ->orWhere([['status','completed'],['procacts.bid_evaluation','<=',$year.'-'.$month.'-31'],['procacts.proceed_notice','>=',$year.'-'.$month.'-01']])
-        ->count();
+            ->join('procacts', 'latest_procact_id', '=', 'procacts.procact_id')
+            ->join('project_activity_status', 'procacts.procact_id', 'project_activity_status.procact_id')
+            ->select('latest_procact_id')
+            ->where([['procacts.bid_evaluation', '<=', $year . '-' . $month . '-31'], ['procacts.proceed_notice', null]])
+            ->orWhere([['status', 'completed'], ['procacts.bid_evaluation', '<=', $year . '-' . $month . '-31'], ['procacts.proceed_notice', '>=', $year . '-' . $month . '-01']])
+            ->count();
 
         $project_reverted = DB::table('project_logs')
-        ->where('log_date','like',$keyword)
-        ->where('project_log_type','like','%reverted')
-        ->count();
+            ->where('log_date', 'like', $keyword)
+            ->where('project_log_type', 'like', '%reverted')
+            ->count();
 
-        $columns = ["Complete","Rebid","Review","On Process","Reverted"];
-        $data = [$project_complete,$project_for_rebid,$project_for_review,$project_onprocess,$project_reverted];
+        $columns = ["Complete", "Rebid", "Review", "On Process", "Reverted"];
+        $data = [$project_complete, $project_for_rebid, $project_for_review, $project_onprocess, $project_reverted];
 
-        return compact('columns','data');
+        return compact('columns', 'data');
     }
+
+    function getUnprocuredProject(Request $request)
+    {
+        $start_year = $request->start_year;
+        $end_year = $request->end_year;
+        $project_count = array();
+        $project_year = array();
+
+        for ($start_year; $start_year <= $end_year; $start_year++) {
+            $dataTable = DB::table('project_timelines')
+                ->join('bid_doc_projects', 'project_timelines.procact_id', 'bid_doc_projects.procact_id')
+                ->join('bid_docs', 'bid_doc_projects.bid_doc_id', 'bid_docs.bid_doc_id')
+                ->join('project_plans', 'bid_doc_projects.procact_id', 'project_plans.latest_procact_id')
+                ->join('project_bidders', 'bid_doc_projects.bid_doc_project_id', 'project_bidders.bid_doc_project_id')
+                ->where('project_year', $start_year)
+                ->where(function (Builder $query) {
+                    $query->where('timeline_status', 'pending')
+                        ->orWhere('bid_status', '!=', 'active')
+                        ->orWhere('bid_status', '!=', 'responsive')
+                        ->orWhere('bid_docs.date_received', 'NULL');
+                })
+                ->get()->count();
+
+            array_push($project_year, $start_year);
+            array_push($project_count, $dataTable);
+        }
+
+        return compact('project_year', 'project_count');
+
+    }
+
+    function getRegSuppProject(Request $request)
+    {
+        $start_year = $request->start_year;
+        $end_year = $request->end_year;
+        $project_reg = array();
+        $project_year = array();
+        $project_supp = array();
+
+        for ($start_year; $start_year <= $end_year; $start_year++) {
+            $dataTable = DB::table('project_plans')
+                ->where([['project_year', $start_year], ['project_type', 'regular']])
+                ->get()->count();
+            array_push($project_reg, $dataTable);
+            $dataTable = DB::table('project_plans')
+                ->where([['project_year', $start_year], ['project_type', 'supplemental']])
+                ->get()->count();
+            array_push($project_supp, $dataTable);
+
+            array_push($project_year, $start_year);
+        }
+
+        return compact('project_year', 'project_reg', 'project_supp');
+
+    }
+
+    function getModeProject(Request $request)
+    {
+        $start_year = $request->start_year;
+        $end_year = $request->end_year;
+        $project_SVP = array();
+        $project_year = array();
+        $project_bidding = array();
+        $project_procurement = array();
+
+        for ($start_year; $start_year <= $end_year; $start_year++) {
+            $dataTable = DB::table('project_plans')
+                ->join('procurement_modes', 'project_plans.mode_id', 'procurement_modes.mode_id')
+                ->where([['project_year', $start_year], ['mode', 'SVP']])
+                ->get()->count();
+            array_push($project_SVP, $dataTable);
+            $dataTable = DB::table('project_plans')
+                ->join('procurement_modes', 'project_plans.mode_id', 'procurement_modes.mode_id')
+                ->where([['project_year', $start_year], ['mode', 'Bidding']])
+                ->get()->count();
+            array_push($project_bidding, $dataTable);
+            $dataTable = DB::table('project_plans')
+                ->join('procurement_modes', 'project_plans.mode_id', 'procurement_modes.mode_id')
+                ->where([['project_year', $start_year], ['mode', 'Negotiated']])
+                ->get()->count();
+            array_push($project_procurement, $dataTable);
+
+            array_push($project_year, $start_year);
+        }
+        return compact('project_year', 'project_SVP', 'project_bidding','project_procurement');
+    }
+
+    function getStatusProjMun(Request $request){
+        $year = $request->year;
+        $status = $request->status;
+        $municipal = $request->municipal;
+
+        $dataTable = DB::table('project_plans')
+        ->join('procacts','project_plans.latest_procact_id','procacts.procact_id')
+        ->join('project_bidders','project_plans.project_bid_id','project_bidders.project_bid')
+        ->join('municipalities','project_plans.municipality_id','municipalities.municipality_id')
+        ->join('projtypes','project_plans.projtype_id','projtypes.projtype_id')
+        ->where([['project_year',$year],['municipality_display',$municipal]])
+        ->get()->toarray();
+
+        if($status == 'complete'){
+            array_map(function($item){
+                $projtype = DB::table('projtypes')
+                ->select('projtype_id')
+                ->get()->toarray();
+                foreach($projtype as $type){
+                    
+                }
+                
+            },$dataTable);
+        }
+    
+
+    }
+
+
 }
--- resources/views/admin/generate_summary_report.blade.php
+++ resources/views/admin/generate_summary_report.blade.php
@@ -26,7 +26,8 @@ tbody:nth-child(odd) {
         <div class="card-body">
           <div>
             <form id="Update_Mon_Yr">
-              <select name="month" id="month">
+            <span>Select Month: </span>
+              <select name="month" id="month_summary">
                 <option value="01">January</option>
                 <option value="02">February</option>
                 <option value="03">March</option>
@@ -40,21 +41,79 @@ tbody:nth-child(odd) {
                 <option value="11">November</option>
                 <option value="12">December</option>
               </select>
-              <select name="year" id="year">
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-                <option value="2023">2023</option>
-                <option value="2024">2024</option>
-                <option value="2025">2025</option>
-                <option value="2026">2026</option>
-                <option value="2027">2027</option>
-                <option value="2028">2028</option>
-                <option value="2029">2029</option>
-                <option value="2030">2030</option>
+              <span>Select Year: </span>
+              <input class="year_pick" name ="year_summary" id="year_summary" style="width: 75px;" type="text">
+            </form>
+            <center><canvas id="summaryreport" style="width:100%;"></canvas></center>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>
+            <form id="Select_Start_End_Year">
+            <span>Select Start-End Year: </span>
+            <input class="year_pick " name ="select_start_year" id="select_start_year" style="width: 75px;" type="text">
+            <span>-</span>
+            <input class="year_pick " name="select_end_year" id="select_end_year" style="width: 75px;" type="text">
+            </form>
+            <center><canvas id="unprocured_project" style="width:100%;"></canvas></center>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>
+            <form id="Select_Start_End_Year_1">
+            <span>Select Start-End Year: </span>
+            <input class="year_pick " name ="select_start_year_1" id="select_start_year_1" style="width: 75px;" type="text">
+            <span>-</span>
+            <input class="year_pick " name="select_end_year_1" id="select_end_year_1" style="width: 75px;" type="text">
+            </form>
+            <center><canvas id="reg_and_supp_project" style="width:100%;"></canvas></center>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>
+            <form id="Select_Start_End_Year_2">
+            <span>Select Start-End Year: </span>
+            <input class="year_pick " name ="select_start_year_2" id="select_start_year_2" style="width: 75px;" type="text">
+            <span>-</span>
+            <input class="year_pick " name="select_end_year_2" id="select_end_year_2" style="width: 75px;" type="text">
+            </form>
+            <center><canvas id="mode_project" style="width:100%;"></canvas></center>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>
+            <form id="Select_Year_Municipal">
+            <span>Select Year: </span>
+            <input class="year_pick " name ="select_start_year_3" id="select_year" style="width: 75px;" type="text">
+            <span>Status: </span>
+            <select name="status" id="status">
+              <option value = "complete">Complete</option>
+              <option value="ongoing">Ongoing</option>
+              <option value="unprocured">Unprocured</option>
+            </select>
+            <span>Municipality: </span>
+            <select name="municipality" id="municipality">
+                <option value="Atok">Atok</option>
+                <option value="Bakun">Bakun</option>
+                <option value="Bokod">Bokod</option>
+                <option value="Buguias">Buguias</option>
+                <option value="Itogon">Itogon</option>
+                <option value="Kabayan">Kabayan</option>
+                <option value="Kapangan">Kapangan</option>
+                <option value="Kibungan">Kibungan</option>
+                <option value="La Trinidad">La Trinidad</option>
+                <option value="Mankayan">Mankayan</option>
+                <option value="Sablan">Sablan</option>
+                <option value="Tuba">Tuba</option>
+                <option value="Tublay">Tublay</option>
+                <option value="ADH">ADH</option>
+                <option value="IDH">IDH</option>
+                <option value="KDH">KDH</option>
+                <option value="NBDH">NBDH</option>
+                <option value="DMDH">DMDH</option>
               </select>
             </form>
-            <center><canvas id="myChart" style="width:100%;max-width:700px"></canvas></center>
+            <center><canvas id="municipal_project" style="width:100%;"></canvas></center>
           </div>
         </div>
       </div>
@@ -64,14 +123,16 @@ tbody:nth-child(odd) {
 
 @endsection
 @push('custom-scripts')
-
 <!--import Chart.js library -->
-<script>src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.1.0/chart.min.js"</script>
+<script>src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.min.js"</script>
+
 <script>
+// Chart 1
+// Summary Report - Doughnut Graph 
 var xValues = [];
 var yValues = [];
 var barColors = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"];
- var chart = new Chart("myChart", {
+var chart = new Chart("summaryreport", {
   type: "doughnut",
   data: {
     labels: xValues,
@@ -89,16 +150,39 @@ var barColors = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65
   }
 });
 
-function get_data(){
-  var date = $("#Update_Mon_Yr").serialize();
-  console.log(date);
+function update_graph(xValues,yValues,barColors){
+  chart.destroy();
+  chart = new Chart("summaryreport", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Project Summary Report"
+    },
+    cutoutPercentage: 60
+  }
+});
+}
 
+function get_data(year){
+
+  if ($("#year_summary").val() ==""){
+    $("#year_summary").val(year);
+
+  }
   var request = $.ajax({
   url: "/get_month_year_report",
   type: "POST",
   data: {
-    year:$("#year").val(),
-    month:$("#month").val()
+    year:$("#year_summary").val(),
+    month:$("#month_summary").val()
   },
   dataType: "JSON",
   headers: {
@@ -112,10 +196,340 @@ function get_data(){
   });
 }
 
-function update_graph(xValues,yValues,barColors){
-  chart.destroy();
-  chart = new Chart("myChart", {
-  type: "doughnut",
+$("#Update_Mon_Yr").on("change", function(){
+  get_data();
+});
+//Chart 2
+//Unprocured Projects - Trend Lines
+var xValues1 = [];
+var yValues1 = [];
+var barColors1 = [];
+var chart1 = new Chart("unprocured_project", {
+  type: "line",
+  data: {
+    labels: xValues1,
+    datasets: [{
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: yValues1
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Yearly Unprocured Projects"
+    },
+  }
+});
+
+function update_unprocured_project(xValues,yValues,barColors){
+  chart1.destroy();
+  chart1 = new Chart("unprocured_project", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues,
+      fill: false,
+      borderColor: 'black',
+      tension: 0
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Yearly Unprocured Projects"
+    },
+    elements: {
+      point:{
+        radius: 7
+      }
+    }
+  }
+});
+}
+
+function get_unprocured_projects(year){
+  //validates null input and compares start_year and end_year
+  if ($("#select_start_year").val() =="" && $("#select_end_year").val() ==""&& $("#select_start_year").val() <= $("#select_end_year").val()){
+    $("#select_start_year").val(year-4);
+    $("#select_end_year").val(year+4);
+  }
+
+  var request = $.ajax({
+  url: "/get_unprocured_project",
+  type: "POST",
+  data: {
+    start_year:$("#select_start_year").val(),
+    end_year:$("#select_end_year").val()
+  },
+  dataType: "JSON",
+  headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+  success: function(data) {
+        xValues1 = data.project_year;
+        yValues1 = data.project_count;
+        update_unprocured_project(xValues1,yValues1,barColors1);
+      }
+  });
+}
+
+$("#Select_Start_End_Year").on("change", function(){
+  get_unprocured_projects();
+});
+
+//Chart 3
+//Regular & Supplemental Projects - Bar Graph
+var xValues2 = [];
+var yValues2 = [];
+var zValues2 = [];
+var barColors1 = [];
+chart2 = new Chart("reg_and_supp_project", {
+  type: "bar",
+  data: {
+    labels: xValues2,
+    datasets: [{
+  label: 'Regular Projects',
+  data: [],
+  backgroundColor: 'rgba(99, 132, 0, 0.6)',
+  borderColor: 'rgba(99, 132, 0, 1)',
+  yAxisID: "y-axis-gravity"
+  },{
+  label: 'Supplemental Projects',
+  data: [],
+  backgroundColor: 'rgba(0, 99, 132, 0.6)',
+  borderColor: 'rgba(0, 99, 132, 1)',
+  yAxisID: "y-axis-density"
+}]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Yearly Accomplished Regular and Supplemental Projects"
+    },
+    scales: {
+    xAxes: [{
+      barPercentage: 1,
+      categoryPercentage: 0.6
+    }],
+    yAxes: [{
+      id: "y-axis-density"
+    }, {
+      id: "y-axis-gravity"
+    }]
+  }
+  }
+});
+function update_reg_supp_project(xValues,yValues,zValues){
+  chart2.destroy();
+  chart2 = new Chart("reg_and_supp_project", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+  label: 'Regular Projects',
+  data: yValues,
+  backgroundColor: 'rgba(99, 132, 0, 0.6)',
+  borderColor: 'rgba(99, 132, 0, 1)',
+  },{
+  label: 'Supplemental Projects',
+  data: zValues,
+  backgroundColor: 'rgba(0, 99, 132, 0.6)',
+  borderColor: 'rgba(0, 99, 132, 1)',
+}]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Yearly Accomplished Projects"
+    },
+    scales: {
+    xAxes: [{
+      barPercentage: 1,
+      categoryPercentage: 0.6
+    }],
+    yAxes: {
+      beginAtZero: true
+    }
+  }
+  }
+});
+}
+
+function get_reg_supp_projects(year){
+  //validates null input and compares start_year and end_year
+  if ($("#select_start_year_1").val() =="" && $("#select_end_year_1").val() ==""&& $("#select_start_year_1").val() <= $("#select_end_year_1").val()){
+    $("#select_start_year_1").val(year-4);
+    $("#select_end_year_1").val(year+4);
+  }
+
+  var request = $.ajax({
+  url: "/get_reg_supp_project",
+  type: "POST",
+  data: {
+    start_year:$("#select_start_year_1").val(),
+    end_year:$("#select_end_year_1").val()
+  },
+  dataType: "JSON",
+  headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+  success: function(data) {
+        xValues2 = data.project_year;
+        yValues2 = data.project_reg;
+        zValues2 = data.project_supp;
+        update_reg_supp_project(xValues2,yValues2,zValues2);
+      }
+  });
+}
+
+$("#Select_Start_End_Year_1").on("change", function(){
+  get_reg_supp_projects();
+});
+
+//Chart 4
+//SVP, Bidding, Negotiated, Procurement Projects - Multiple Line Graph
+
+var xValues3 = [];
+var yValues3 = [];
+var zValues3 = [];
+var aValues3 = [];
+
+var chart3 = new Chart("mode_project", {
+  type: "line",
+  data: {
+    labels: xValues3,
+    datasets: [{
+      label: 'SVP',
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: yValues3
+    },{
+      label: 'Bidding',
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: zValues3
+    },{
+      label: 'Negotiated',
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: aValues3
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Total Modes of Projects"
+    },
+  }
+});
+
+function update_mode_project(xValues,yValues,zValues,aValues){
+  chart3.destroy();
+  chart3 = new Chart("mode_project", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      label: 'SVP',
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: yValues
+    },{
+      fill: false,
+      label: 'Bidding',
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: zValues
+    },{
+      fill: false,
+      label: 'Negotiated',
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+      data: aValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Total Modes of Projects"
+    },
+    elements: {
+      point:{
+        radius: 7
+      }
+    }
+  }
+});
+}
+
+function get_mode_projects(year){
+  //validates null input and compares start_year and end_year
+  if ($("#select_start_year_2").val() =="" && $("#select_end_year_2").val() ==""&& $("#select_start_year_2").val() <= $("#select_end_year_2").val()){
+    $("#select_start_year_2").val(year-4);
+    $("#select_end_year_2").val(year+4);
+  }
+
+  var request = $.ajax({
+  url: "/get_mode_project",
+  type: "POST",
+  data: {
+    start_year:$("#select_start_year_2").val(),
+    end_year:$("#select_end_year_2").val()
+  },
+  dataType: "JSON",
+  headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+  success: function(data) {
+        xValues3 = data.project_year;
+        yValues3 = data.project_SVP;
+        zValues3 = data.project_bidding;
+        aValues3 = data.project_procurement;
+        update_mode_project(xValues3,yValues3,zValues3,aValues3);
+      }
+  });
+}
+
+$("#Select_Start_End_Year_2").on("change", function(){
+  get_mode_projects();
+});
+
+//Chart 5
+//Project Type per Municipality with Status - Pie Chart
+
+var xValues4 = [];
+var yValues4 = [];
+var barColors4 = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"];
+var chart4 = new Chart("municipal_project", {
+  type: "pie",
+  data: {
+    labels: xValues4,
+    datasets: [{
+      backgroundColor: barColors4,
+      data: yValues4
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Projects Status per Municipality"
+    },
+    cutoutPercentage: 60
+  }
+});
+
+function update_graph_municipal(xValues,yValues,barColors){
+  chart4.destroy();
+  chart4 = new Chart("municipal_project", {
+  type: "pie",
   data: {
     labels: xValues,
     datasets: [{
@@ -126,67 +540,60 @@ function update_graph(xValues,yValues,barColors){
   options: {
     title: {
       display: true,
-      text: "Project Summary Report"
+      text: "Projects Status per Municipality"
     },
     cutoutPercentage: 60
   }
 });
 }
 
+function get_proj_status_mun(year){
 
-$("#month").on("change", function(){
-   get_data();
-});
+  if ($("#select_year").val() ==""){
+    $("#select_year").val(year);
 
-$("#year").on("change", function(){
-   get_data();
-});
-
-</script>
-
-<script>
-$(".datepicker").datepicker({
-  format: 'mm/dd/yyyy',
-});
-
-var table= $('#app_table').DataTable(
-  {
-    language: {
-      paginate: {
-        next: '<i class="fas fa-angle-right">',
-        previous: '<i class="fas fa-angle-left">'
-      }
-    },
-    pageLength: 100,
-    rowGroup: {
-      startRender: function ( rows, group ) {
-        return group;
-      },
-      dataSrc: 1
-    },
-    columnDefs: [
-      {
-        targets: [0,1,2,3,4,5,6,7,8,9],
-        orderable: false
-      },
-      {
-        targets: [0,1],
-        visible: false
-      }
-    ],
-    rowsGroup: [0,1,2,3,4,5,6,7],
   }
-);
+  var request = $.ajax({
+  url: "/get_proj_status_mun",
+  type: "POST",
+  data: {
+    year:$("#select_year").val(),
+    status:$("#status").val(),
+    municipal:$("#municipality").val()
+  },
+  dataType: "JSON",
+  headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+  success: function(data) {
+        xValues4 = data.columns;
+        yValues4 = data.data;
+        update_graph_municipal(xValues4,yValues4,barColors4);
+      }
+  });
+}
 
-// remove duplicate group header
-var seen = {};
-$('.dtrg-group').each(function() {
-  var txt = $(this).text();
-  if (seen[txt])
-  $(this).remove();
-  else
-  seen[txt] = true;
+$("#Select_Year_Municipal").on("change", function(){
+  get_proj_status_mun();
 });
 
+//Customizes Data Picker
+$('.year_pick').datepicker({
+         minViewMode: 2,
+         format: 'yyyy'
+       });
+//Loads all functions
+$( document ).ready(function() {
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+  const d = new Date();
+  let year = d.getFullYear();
+
+  get_data(year);
+  get_unprocured_projects(year);
+  get_reg_supp_projects(year);
+  get_mode_projects(year);
+});
 </script>
+
 @endpush
